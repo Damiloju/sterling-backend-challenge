04afb79842d97bbe2a97b4a5013343ecd7f69526
Yusuf Damiloju <yusufdamiloju@gmail.com>
2020-07-14 20:44:27 +0100

    add routes for user signup

---
+++ src/controllers/index.js
@@ -0,0 +1,8 @@
+/**
+ * Bundle all controllers into a controller object
+ */
+const controller = {}; // Define the controller object ...
+
+controller.userController = require('./UserCotroller');
+
+module.exports = controller;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const userRoutes = require('./user');
+
+router.use('/users', userRoutes);
+
 // GET home page.
 router.get('/', (req, res) => res.send({ msg: 'Evisit Portal Api 1.0' }));
 
