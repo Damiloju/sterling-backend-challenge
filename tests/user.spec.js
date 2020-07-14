5829d7a8157d5574b2f12a0186fdac3c5836d0f4
Yusuf Damiloju <yusufdamiloju@gmail.com>
2020-07-14 22:17:53 +0100

    wip

--- tests/user.spec.js
+++ tests/user.spec.js
@@ -6,54 +6,56 @@ const { setUpDatabase } = require('./fixtures/db');
 /* eslint-disable no-undef */
 beforeEach(setUpDatabase);
 
-test('should signup a new user', async () => {
-  const response = await request(app)
-    .post('/api/v1/users')
-    .send({
-      username: 'dongun',
-      email: 'dami@example.com',
-      password: 'Mypass1020',
-    })
-    .expect(201);
+describe('User SignUp', () => {
+  test('should signup a new user', async () => {
+    const response = await request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'dongun',
+        email: 'dami@example.com',
+        password: 'Mypass1020',
+      })
+      .expect(201);
 
-  // eslint-disable-next-line no-underscore-dangle
-  const user = await User.findById(response.body.user._id);
-  expect(user).not.toBeNull();
+    // eslint-disable-next-line no-underscore-dangle
+    const user = await User.findById(response.body.user._id);
+    expect(user).not.toBeNull();
 
-  expect(response.body).toMatchObject({
-    user: {
-      username: 'dongun',
-      email: 'dami@example.com',
-    },
+    expect(response.body).toMatchObject({
+      user: {
+        username: 'dongun',
+        email: 'dami@example.com',
+      },
+    });
   });
-});
 
-test('should not signup a new user with invalid email', async () => {
-  await request(app)
-    .post('/api/v1/users')
-    .send({
-      username: 'don',
-      email: 'dami',
-      password: 'Mypass1020',
-    })
-    .expect(400);
+  test('should not signup a new user with invalid email', async () => {
+    await request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'don',
+        email: 'dami',
+        password: 'Mypass1020',
+      })
+      .expect(400);
 
-  // eslint-disable-next-line no-underscore-dangle
-  const user = await User.findOne({ username: 'don' });
-  expect(user).toBeNull();
-});
+    // eslint-disable-next-line no-underscore-dangle
+    const user = await User.findOne({ username: 'don' });
+    expect(user).toBeNull();
+  });
 
-test('should not signup a new user with invalid username', async () => {
-  await request(app)
-    .post('/api/v1/users')
-    .send({
-      username: 'do',
-      email: 'dami@dami.com',
-      password: 'Mypass1020',
-    })
-    .expect(400);
+  test('should not signup a new user with invalid username', async () => {
+    await request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'do',
+        email: 'dami@dami.com',
+        password: 'Mypass1020',
+      })
+      .expect(400);
 
-  // eslint-disable-next-line no-underscore-dangle
-  const user = await User.findOne({ username: 'do' });
-  expect(user).toBeNull();
+    // eslint-disable-next-line no-underscore-dangle
+    const user = await User.findOne({ username: 'do' });
+    expect(user).toBeNull();
+  });
 });
