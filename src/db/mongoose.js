085092666bb17fb19844ec234f2422a046cad357
Yusuf Damiloju <yusufdamiloju@gmail.com>
2020-07-14 21:08:38 +0100

    change connection url in mongoose and add middleware

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
-const connectionURL = process.env.MONGODB_URL;
+const { MONGO_USERNAME } = process.env;
+const { MONGO_PASSWORD } = process.env;
+const { MONGO_HOSTNAME } = process.env;
+const { MONGO_PORT } = process.env;
+const { MONGO_DB } = process.env;
+
+const connectionURL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 mongoose.connect(connectionURL, {
   useNewUrlParser: true,
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { userController } = require('../controllers');
+const checkDBConnection = require('../middlewares/checkDBConnection');
 
 const router = express.Router();
 
 // Create a new User.
-router.post('', userController.createUser);
+router.post('', [checkDBConnection], userController.createUser);
 
 module.exports = router;
