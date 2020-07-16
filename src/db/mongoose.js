8888516859170f2d3fddb92d9010fab11a23aa70
Yusuf Damiloju <yusufdamiloju@gmail.com>
2020-07-16 13:29:08 +0100

    testing

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -19,10 +19,14 @@ const options = {
   useFindAndModify: true,
 };
 
-let connectionURL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+let connectionURL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin?retryWrites=true&w=majority`;
+
+if (process.env.NODE_ENV === 'production') {
+  connectionURL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin?retryWrites=true&w=majority`;
+}
 
 if (process.env.NODE_ENV === 'test') {
-  connectionURL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB_TEST}?authSource=admin`;
+  connectionURL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB_TEST}?authSource=admin?retryWrites=true&w=majority`;
 }
 
 mongoose
