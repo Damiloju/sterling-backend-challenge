0bf09cc6ee097f1c21185f7ad968f8cbbc7dea84
Yusuf Damiloju <yusufdamiloju@gmail.com>
2020-07-16 13:12:14 +0100

    fix url issue

--- src/app.js
+++ src/app.js
@@ -10,16 +10,9 @@ const expressWinston = require('express-winston');
 const session = require('express-session');
 const redis = require('redis');
 
-let redisClient;
-if (process.env.REDIS_HOST) {
-  redisClient = redis.createClient({
-    host: process.env.REDIS_HOST || '127.0.0.1',
-  });
-} else if (process.env.REDIS_URL) {
-  redisClient = redis.createClient({
-    url: process.env.REDIS_URL,
-  });
-}
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+});
 
 const RedisStore = require('connect-redis')(session);
 const winstonOptions = require('./config/winston');
